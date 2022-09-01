73e1f488272cd47a21834fab8714253be3e001c1
srshreyash <106814401+srshreyash@users.noreply.github.com>
2022-09-01 22:08:03 +0100

    Time Series Analysis - Final Project - Dashboard - First Commit

---
+++ FinalProject/Main.py
@@ -0,0 +1,94 @@
+import os
+import time
+import datetime
+import streamlit as st
+import yahoo_fin.stock_info as si
+from TSA_Models import auto_arima, fb_prophet, LSTM_Model
+
+def print_metrics():
+    predef_train_from = "01/01/2015"
+    predef_train_to = "12/31/2021"
+    predef_predict_from = "07/01/2022"
+    predef_predict_to = "07/18/2022"
+    st.write(f"Training period : {predef_train_from} to {predef_train_to}")
+    st.write(f"Prediction period : {predef_predict_from} to {predef_predict_to}")
+    st.write("Please click on the Train button below to proceed further")
+
+def create_form(all_stocks, formname = "Default", button_label = "Default", disabled = False):
+    genericform = st.form(key = formname)
+    stock_selector = genericform.selectbox(
+        "Select a Ticker",
+        all_stocks, disabled=disabled)
+    ss_idx = all_stocks.index(stock_selector)
+    d = genericform.date_input(
+        "Please select the Time Period for Train/Test Data",
+        value = (datetime.date(2001, 1, 1), datetime.date.today()),
+        min_value = datetime.date(2001, 1, 1),
+        max_value = datetime.date.today(), disabled = disabled)
+    formbutton = genericform.form_submit_button(label=button_label)
+    return formbutton
+
+st.set_page_config(page_title="ML for Time Series Forecasting", page_icon="📈", layout="wide", initial_sidebar_state="collapsed", menu_items=None)
+st.title("Application of Machine Learning Models for Time Series Analysis")
+
+predef = st.checkbox("Use predefined Metrics", help = "Please select this to use the predefined metrics for model training and results")
+
+if predef:
+    print_metrics()
+    path = os.path.dirname(__file__)
+    train_all = st.button("Train All")
+else:
+    train_all = False
+arima_tab, fbp_tab, LSTM_tab= st.tabs(["ARIMA","FBProphet","LSTM"])
+all_stocks = si.tickers_nifty50()
+
+with arima_tab:
+    if predef:
+        arimabutton = create_form(all_stocks=all_stocks,formname = "arima", button_label = "Train ARIMA", disabled=True)
+        model = "..\SavedModels\ARIMA_model.pkl"
+    if not(predef):
+        arimabutton = create_form(all_stocks=all_stocks,formname = "arima", button_label = "Train ARIMA", disabled=False)
+        model = None
+    if arimabutton:
+        st.write("SRS")
+        auto_arima(model_path = model, use_predefined_metrics = predef)
+
+with fbp_tab:
+    if predef:
+        fbpbutton = create_form(all_stocks=all_stocks,formname = "fbpform", button_label = "Train FBProphet", disabled=True)
+        model = "..\SavedModels\FBProphet_model.pkl"
+    if not(predef):
+        fbpbutton = create_form(all_stocks=all_stocks,formname = "fbpform", button_label = "Train FBProphet", disabled=False)
+        model = None
+    if fbpbutton:
+        fb_prophet(model_path = model, use_predefined_metrics = predef)
+
+with LSTM_tab:
+    if predef:
+        lstmbutton = create_form(all_stocks=all_stocks,formname = "LSTMform", button_label = "Train LSTM", disabled = True)
+        model = "..\SavedModels\LSTM_model.pkl"
+    if not(predef):
+        lstmbutton = create_form(all_stocks=all_stocks,formname = "LSTMform", button_label = "Train LSTM", disabled = False)
+        model = None
+    if lstmbutton:
+        LSTM_Model(model_path = model, use_predefined_metrics = predef)
+    
+if train_all:
+    st.header("ARIMA Modelling: ")
+    st.write("Please find the results of ARIMA modelling below: ")
+    start_time = time.time()
+    model_path = path+'/SavedModels/ARIMA_model.pkl'
+    auto_arima(model_path = model_path, use_predefined_metrics = predef)
+    st.write("Total Time taken : %s seconds" % (time.time() - start_time))
+    st.header("FBProphet Modelling: ")
+    st.write("Please find the results of FBProphet modelling below: ")
+    start_time = time.time()
+    model_path = path+'/SavedModels/FBProphet_model.pkl'
+    fb_prophet(model_path = model_path, use_predefined_metrics = predef)
+    st.write("Total Time taken : %s seconds" % (time.time() - start_time))
+    st.header("LSTM Modelling: ")
+    st.write("Please find the results of LSTM modelling below: ")
+    start_time = time.time()
+    model_path = path+'/SavedModels'
+    LSTM_Model(model_path = model_path, use_predefined_metrics = predef)
+    st.write("Total Time taken : %s seconds" % (time.time() - start_time))
---
+++ FinalProject/TSA_Models.py
@@ -0,0 +1,211 @@
+import pickle
+import numpy as np
+import pandas as pd
+import pmdarima as pm
+import streamlit as st
+from prophet import Prophet
+import matplotlib.pyplot as plt
+from pandas.plotting import lag_plot
+from datetime import datetime, timedelta
+from yahoo_fin.stock_info import get_data
+from keras.layers import LSTM, Dense, Dropout
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from keras.models import Sequential, model_from_json
+
+def auto_arima(stock_selector = "RELIANCE.NS", use_predefined_metrics = True, traintest_dates=["01/01/2015", "12/31/2021"],
+               predict_dates = ["01/01/2022","07/18/2022"], predict_days = 185, lag = 1, model_path = None):
+    
+    traintest_date_from = traintest_dates[0]
+    traintest_date_to = traintest_dates[1]
+    predict_date_from = predict_dates[0]
+    predict_date_to = predict_dates[1]
+    predict_date_from = pd.to_datetime(traintest_date_to) + timedelta(days = 1)
+    predict_date_to = pd.to_datetime(traintest_date_to) + timedelta(days = predict_days)
+    train_df = get_data(stock_selector, start_date = traintest_date_from, end_date = traintest_date_to, index_as_date = True, interval="1d")
+    predict_df = get_data(stock_selector, start_date = predict_date_from, end_date = predict_date_to, index_as_date = True, interval="1d")
+    df = train_df.copy()
+    idx = pd.date_range(df.index.min(), df.index.max())
+    df = df.reindex(idx)
+    df["close"] = df["close"].interpolate(method="linear")
+    st.line_chart(df["close"])
+    fig = plt.figure(figsize = (10, 5))
+    lag_plot(df['close'], lag=lag)
+    plt.title(f"{stock_selector} - Autocorrelation plot with lag = {lag}")
+    st.pyplot(fig)
+    if use_predefined_metrics:
+      model = pickle.load(open(model_path, 'rb'))
+    if not(use_predefined_metrics):
+      model = pm.auto_arima(df.close, start_p=1, start_q=1,
+                    test='adf',       # use adftest to find optimal 'd'
+                    max_p=3, max_q=3, # maximum p and q
+                    m=1,              # frequency of series
+                    d=None,           # let model determine 'd'
+                    seasonal=False,   # No Seasonality
+                    start_P=0, 
+                    D=0, 
+                    trace=True,
+                    error_action='ignore',  
+                    suppress_warnings=True, 
+                    stepwise=True)
+      pickle.dump(model, open(model_path, 'wb')) #--- Save Model
+    st.write(model.summary())
+    fig = model.plot_diagnostics(figsize=(10,9))
+    st.pyplot(fig)
+    n_periods = pd.to_datetime(predict_date_to) - pd.to_datetime(predict_date_from)
+    n_periods = n_periods.days
+    n_periods = predict_days
+    fc, confint = model.predict(n_periods=n_periods, return_conf_int=True) #Adding 1 because difference doesn't include last date
+    df["date"] = df.index
+    index_of_fc = np.arange(len(df.close), len(df.close)+n_periods)
+    index_of_fc = pd.date_range(start=df["date"].iloc[-1], periods=n_periods+1, freq='d', closed='right')
+    fc_series = pd.Series(fc, index=index_of_fc)
+    lower_series = pd.Series(confint[:, 0], index=index_of_fc)
+    upper_series = pd.Series(confint[:, 1], index=index_of_fc)
+    fig = plt.figure(figsize = (10, 5))
+    plt.plot(df.close, label="Train Data - Actual")
+    plt.plot(predict_df.close, label="Actual Values of Prediction")
+    plt.plot(fc_series, color='darkgreen', label = "Prediction Values")
+    plt.fill_between(lower_series.index, 
+                      lower_series, 
+                      upper_series, 
+                      color='k', alpha=.15, label="Prediction Range")
+    plt.legend()
+    plt.title("Final Forecast")
+    st.pyplot(fig)
+    fig = plt.figure(figsize = (10, 5))
+    plt.plot(predict_df.close, label="Actual Values of Prediction")
+    plt.plot(fc_series, color='darkgreen', label = "Prediction Values")
+    plt.fill_between(lower_series.index, 
+                      lower_series, 
+                      upper_series, 
+                      color='k', alpha=.15, label="Prediction Range")
+    plt.legend()
+    st.pyplot(fig)
+
+def LSTM_Model(stock_selector = "RELIANCE.NS" , use_predefined_metrics = True, traintest_dates=["01/01/2015", "12/31/2021"],
+               predict_dates = ["01/01/2022","07/18/2022"], model_path = None, n=3):
+
+    stock = stock_selector
+    train_df = get_data(stock, start_date=traintest_dates[0], end_date=traintest_dates[1], index_as_date = True, interval="1d")
+    predict_df = get_data(stock, start_date=predict_dates[0], end_date=predict_dates[1], index_as_date = True, interval="1d")
+    df = train_df.copy()
+    df["returns"] = df.close.pct_change()
+    df["log_returns"] = np.log(1+ df["returns"])
+    fig = plt.figure(figsize = (10, 5))
+    plt.plot(df.log_returns, label = "Log Returns")
+    plt.legend()
+    st.pyplot(fig)
+    df.dropna(inplace = True)
+    X = df[["close","log_returns"]]
+    scaler = MinMaxScaler(feature_range=(0,1)).fit(X)
+    X_scaled = scaler.transform(X)
+    y = [x[0] for x in X_scaled]
+    split = int(len(X_scaled)*0.8)
+    X_train = X_scaled[:split]
+    X_test = X_scaled[split:len(X_scaled)]
+    Y_train = y[:split]
+    Y_test = y[split:len(y)]
+    n = int(n)
+    Xtrain=[]
+    ytrain = []
+    Xtest = []
+    ytest = []
+    for i in range(n, len(X_train)):
+      Xtrain.append(X_train[i-n : i, :X_train.shape[1]])
+      ytrain.append(Y_train[i])
+    for i in range(n, len(X_test)):
+      Xtest.append(X_test[i-n:i, :X_test.shape[1]])
+      ytest.append(Y_test[i])
+    Xtrain, ytrain = (np.array(Xtrain), np.array(ytrain))
+    Xtrain = np.reshape(Xtrain, (Xtrain.shape[0], Xtrain.shape[1], Xtrain.shape[2]))
+    Xtest, ytest = (np.array(Xtest), np.array(ytest))
+    Xtest = np.reshape(Xtest, (Xtest.shape[0], Xtest.shape[1], Xtest.shape[2]))
+    if use_predefined_metrics:
+        json_file = open(model_path+"\LSTM_model.json", 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        model = model_from_json(loaded_model_json)
+        model.load_weights(model_path+"\LSTM_model.h5")
+        model.compile(loss='mean_squared_error', optimizer='adam')#, metrics=['accuracy'])
+    if not(use_predefined_metrics):
+        model = Sequential()
+        ##        model.add(LSTM(4, input_shape = (Xtrain.shape[1], Xtrain.shape[2])))
+        ##        model.add(Dense(1))
+        model.add(LSTM(units = 50, return_sequences = True, input_shape = (Xtrain.shape[1], Xtrain.shape[2])))
+        model.add(Dropout(0.2))
+
+        model.add(LSTM(units = 50, return_sequences = True))
+        model.add(Dropout(0.2))
+
+        model.add(LSTM(units = 50, return_sequences = True))
+        model.add(Dropout(0.2))
+
+        model.add(LSTM(units = 50))
+        model.add(Dropout(0.2))
+
+        model.add(Dense(units = 1))
+        model.compile(loss="mean_squared_error", optimizer = "adam")
+        early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, mode='min')
+        model.fit(Xtrain, ytrain, epochs=100, validation_data=(Xtest, ytest), batch_size=16, verbose =1, callbacks = [early_stopping])
+    model.summary(print_fn=lambda x: st.text(x))
+    trainPredict = model.predict(Xtrain)
+    testPredict = model.predict(Xtest)
+    trainPredict = np.c_[trainPredict, np.zeros(trainPredict.shape)]
+    testPredict = np.c_[testPredict, np.zeros(testPredict.shape)]
+    trainPredict = scaler.inverse_transform(trainPredict)
+    trainPredict = [x[0] for x in trainPredict]
+    testPredict = scaler.inverse_transform(testPredict)
+    testPredict = [x[0] for x in testPredict]
+    trainScore = mean_squared_error(list(df["close"][n:split]), trainPredict, squared=False)
+    st.write(f"Train Score: {trainScore}")
+    testScore = mean_squared_error(list(df["close"][split+n:]), testPredict, squared=False)
+    st.write(f"Test Score: {testScore}")
+    dff = pd.DataFrame({"actual":list(df["close"][n:split]),"predicted":trainPredict})
+    fig = plt.figure(figsize = (10, 5))
+    plt.plot(dff["actual"], label="Actual")
+    plt.plot(dff["predicted"], label="Predicted")
+    plt.title("Model Performance on Train Data: ")
+    plt.legend()
+    st.pyplot(fig)
+    dff = pd.DataFrame({"actual":list(df["close"][split+n:]),"predicted":testPredict})
+    fig = plt.figure(figsize = (10, 5))
+    plt.plot(dff["actual"], label = "Actual")
+    plt.plot(dff["predicted"], label="Predicted")
+    plt.title("Model Performance on Test Data: ")
+    plt.legend()
+    st.pyplot(fig)
+
+def fb_prophet(stock_selector = "RELIANCE.NS" , use_predefined_metrics = True, traintest_dates=["01/01/2015", "12/31/2021"],
+               predict_dates = ["01/01/2022","07/18/2022"], model_path = None):
+
+    train_df = get_data(stock_selector, start_date = traintest_dates[0], end_date = traintest_dates[1], index_as_date = False, interval="1d")
+    predict_df = get_data(stock_selector, start_date = predict_dates[0], end_date = predict_dates[1], index_as_date = False, interval="1d")
+    df = train_df[["date","close"]]
+    df.columns = ["ds","y"]
+    df['ds']= pd.to_datetime(df['ds'])
+    if use_predefined_metrics:
+      model = pickle.load(open(model_path, 'rb'))
+    if not(use_predefined_metrics):
+        model = Prophet()
+        mod = model.fit(df)
+    df["ds"].min()
+    pred = pd.DataFrame()
+    pred["ds"] = df["ds"]
+    predout = model.predict(pred)
+    fig = model.plot(predout)
+    plt.title("Training: ")
+    plt.xlabel("Dates")
+    plt.ylabel("Price")
+    plt.legend(["Actual","Predicted","Confidence Interval"])
+    st.pyplot(fig)
+    pr = pd.DataFrame()
+    pr["ds"] = list(pd.date_range("09/01/2022","09/30/2023"))
+    prout = model.predict(pr)
+    fig = model.plot(prout)
+    plt.title("Prediction: ")
+    plt.xlabel("Dates")
+    plt.ylabel("Price")
+    plt.legend(["Actual","Predicted","Confidence Interval"])
+    st.pyplot(fig)
+
